7128713dc75120fa5c97df3ee837e63da4a58aaf
Logan Sherwin <lsherwin10@gmail.com>
2026-07-26 21:55:40 -0700

    Update vite config to support custom domain

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/boom-yeah/', // Matches your repository name so assets/CSS load properly
+  base: '/',
 });
